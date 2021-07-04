ea09aaf2fd9bd2f26e731c9f1d93671bfc2860f6
Pandam <victorvgv96@hotmail.es>
2021-07-04 22:07:33 +0200

    Hola mundo!

---
+++ src/test/java/com/badges/APIBadges/BadgesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badges.APIBadges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
